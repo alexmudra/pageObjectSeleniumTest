e5f075b6aec812272ffd4d8d0c1d47aaf53f7016
alexmudra <alexander.kobko@gmail.com>
2018-06-11 22:35:21 +0300

    add .gitignore

---
+++ Tests-Selenium-Advanced/src/test/java/MyFirstTest.java
@@ -0,0 +1,53 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class MyFirstTest {
+
+        private WebDriver driver;
+        private WebDriverWait wait;
+
+    @Before
+            public void start(){
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+
+        //System.setProperty("webdriver.gecko.driver", "D:\\workspace\\geckodriver\\geckodriver.exe");
+        driver.navigate().to("http://www.google.com");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.name("q")).sendKeys("webdriver");
+        driver.findElement(By.name("btnG")).click();
+        wait.until(titleIs("webdriver - Поиск в Google"));
+    }
+
+    /*@Test
+    public void mySecondTest() {
+        driver.navigate().to("http://www.google.com");
+        wait.until((WebDriver d) -> d.findElement(By.name("q"))).sendKeys("webdriver");
+        driver.findElement(By.name("btnG")).click();
+        wait.until(titleIs("webdriver - Поиск в Google"));
+    }
+
+    @Test
+    public void myThirdTest() {
+        driver.navigate().to("http://www.google.com");
+        driver.findElement(By.name("q")).sendKeys("webdriver");
+        driver.findElement(By.name("btnG")).click();
+        wait.until(titleIs("webdriver - Поиск в Google"));
+
+    }*/
+     @After
+     public void stop(){
+         driver.quit();
+         driver = null;
+
+    }
+}
